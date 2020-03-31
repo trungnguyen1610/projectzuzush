f37d6f0dbb13be48c376d56dc5e0267fbbf203fc
hdhai91 <hdhai91@gmail.com>
2020-03-31 11:12:28 +0700

    up new source

---
+++ assets/js/import.js
@@ -0,0 +1,7 @@
+$.getScript("assets/lib/fontawesome-free-5.13.0-web/js/all.min.js", function() {
+    console.log( "Load js fontawesome-free-5.13.0-web" );
+});
+
+$.getScript("assets/lib/bootstrap-4.4.1/js/bootstrap.min.js", function() {
+    console.log( "Load js bootstrap-4.4.1" );
+});
