6245363602270b4a9887d7f24e55642d998a411e
TuyenNguyen <nguyentuyen1322@gmail.com>
2020-04-01 16:21:45 +0700

    Ghép thành công soure zuzuhs đã hoàn thành các phần đầy đủ home trang zuzuhs html + css + js

--- assets/js/home.js
+++ assets/js/home.js
@@ -31,3 +31,27 @@ $(window).scroll(function() {
 	});
 });
  // Difference - end
+ 
+ // Solution - start
+ $(window).scroll(function() {
+
+    if ($(this).scrollTop()>1000)
+     {
+        $('.vsi-fe3-animate-left-1').addClass("vsi-fe3-our-solutions__left-1");
+     }
+   
+     if ($(this).scrollTop()>1000)
+     {
+        $('.vsi-fe3-animate-left-2').addClass("vsi-fe3-our-solutions__left-2");
+     }
+     if ($(this).scrollTop()>1000)
+     {
+        $('.vsi-fe3-animate-right-1').addClass("vsi-fe3-our-solutions__right-1");
+     }
+     if ($(this).scrollTop()>1000)
+     {
+        $('.vsi-fe3-animate-right-2').addClass("vsi-fe3-our-solutions__right-2");
+     }
+ });
+
+ // Solution - end
