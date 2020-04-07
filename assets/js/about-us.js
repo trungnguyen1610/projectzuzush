e7b9fee0a168ee0a21c06d287c590347896e81b0
TuyenNguyen <nguyentuyen1322@gmail.com>
2020-04-07 11:54:03 +0700

    code done pages about-us hoàn thành giao diện trang đang đợi QC html css js

--- assets/js/about-us.js
+++ assets/js/about-us.js
@@ -38,3 +38,10 @@ $(document).ready(function() {
       $(this).find('.vsi-fe1-text-content').removeClass("vsi-fe1-active-text");     
   });
 });
+$(document).ready(function() {     
+  $(".vsi-fe1-show").hover(function() {
+      $(this).find('.vsi-fe1-icon-down').addClass("vsi-fe1-icon-block");    
+  }, function() {    
+      $(this).find('.vsi-fe1-icon-down').removeClass("vsi-fe1-icon-block");     
+  });
+});
