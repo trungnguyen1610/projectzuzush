aee279be1b6ba463d1ea8bb4f747c5016e92d868
HuyHoang15982 <hoang.hdh280396@gmail.com>
2020-04-08 20:00:03 +0700

    Up code animation header

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,15 +1,56 @@
 // js header - start 
-$(window).scroll(function () {
+// $(window).scroll(function () {
 
-   if ($(this).scrollTop() > 0) {
-       $('.vsi-fe3-my-header').addClass("vsi-fe3__header--animation");
+//    if ($(this).scrollTop() > 0) {
+//        $('.vsi-fe3-my-header').addClass("vsi-fe3__header--animation");
+//    }
+//    else {
+//        $('.vsi-fe3-my-header').removeClass("vsi-fe3__header--animation");
+//    }
+// });
+
+
+
+// Animation scroll haeder  
+var lastScrollTop = 0;
+$(window).scroll(function(event){
+   var st = $(this).scrollTop();
+   if (st > lastScrollTop){
+    if ( !$(".vsi-fe3-header-animation").hasClass("vsi-fe3-header-animation-hide")) {
+        $(".vsi-fe3-header-animation").addClass("vsi-fe3-header-animation-hide");
+        $(".vsi-fe3-header-animation").removeClass("vsi-fe3-header-animation-show");
+    } 
+   } else {
+    if ( !$(".vsi-fe3-header-animation").hasClass("vsi-fe3-header-animation-show")) {
+        $(".vsi-fe3-header-animation").removeClass("vsi-fe3-header-animation-hide");
+        $(".vsi-fe3-header-animation").addClass("vsi-fe3-header-animation-show");
+    } 
    }
-   else {
-       $('.vsi-fe3-my-header').removeClass("vsi-fe3__header--animation");
-   }
+   lastScrollTop = st;
 });
 
 
+var lastScrollTopMB = 0;
+$(window).scroll(function(event){
+   var st = $(this).scrollTop();
+   if (st > lastScrollTopMB){
+    if ( !$(".vsi-fe3-header-mobile-animation").hasClass("vsi-fe3-header-mobile-animation-hide")) {
+        $(".vsi-fe3-header-mobile-animation").addClass("vsi-fe3-header-mobile-animation-hide");
+        $(".vsi-fe3-header-mobile-animation").removeClass("vsi-fe3-header-mobile-animation-show");
+        console.log("down")
+    } 
+   } else {
+    if ( !$(".vsi-fe3-header-mobile-animation").hasClass("vsi-fe3-header-mobile-animation-show")) {
+        $(".vsi-fe3-header-mobile-animation").removeClass("vsi-fe3-header-mobile-animation-hide");
+        $(".vsi-fe3-header-mobile-animation").addClass("vsi-fe3-header-mobile-animation-show");
+        console.log("up")
+    } 
+   }
+   lastScrollTopMB = st;
+});
+
+
+//Click btn menu mobile
 
 $("#vsi-fe3-btn-menu").click(function () {
    $(".vsi-fe3-my-navbarNav-mobile").addClass("vsi-fe3-animation-menu-mobile ");
@@ -18,11 +59,13 @@ $("#vsi-fe3-btn-menu").click(function () {
 $("#vsi-fe3-btn-menu").click(function () {     
    if ( !$(".vsi-fe3-my-navbar-mobile-hide").hasClass("vsi-fe3-my-navbar-mobile-show")) {
        $(".vsi-fe3-my-navbar-mobile-hide").addClass("vsi-fe3-my-navbar-mobile-show");
+     
    } 
    else if ($(".vsi-fe3-my-navbar-mobile-hide").hasClass("vsi-fe3-my-navbar-mobile-show")) {
        $(".vsi-fe3-my-navbar-mobile-hide").removeClass("vsi-fe3-my-navbar-mobile-show");
    }
 });
+
 
 $("#vsi-fe3-btn-menu").click(function () {     
    if ( !$(".vsi-fe3-my-navbarNav-footer-mobile-hide").hasClass("vsi-fe3-my-navbarNav-footer-mobile-show")) {
@@ -39,6 +82,8 @@ $("#vsi-fe3-btn-menu").click(function () {
      console.log("a");
    if ( !$(".vsi-fe3-menu-mobile--hide").hasClass("vsi-fe3-menu-mobile--show")) {
        $(".vsi-fe3-menu-mobile--hide").addClass("vsi-fe3-menu-mobile--show");
+       $(".vsi-fe3-header-mobile-animation").removeClass("vsi-fe3-header-mobile-animation-show");
+       $(".vsi-fe3-header-animation").removeClass("vsi-fe3-header-animation-show");
    } 
    else if ($(".vsi-fe3-menu-mobile--hide").hasClass("vsi-fe3-menu-mobile--show")) {
        $(".vsi-fe3-menu-mobile--hide").removeClass("vsi-fe3-menu-mobile--show");
